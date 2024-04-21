099404ddef39bdc4b10300fb744432d78d4b5739
MykolaServetnyk <mikokobass@gmail.com>
2024-04-21 14:59:06 +0300

    init

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../src/components/App/App'
+import './index.css'
+@import 'node_modules/modern-normalize/modern-normalize.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
